625d0731c044b68efca7e6a73e118562d0bb8040
Mauricio Costa <mauriciocostaemail@gmail.com>
2019-10-20 10:10:53 -0300

    commit inicial

---
+++ ProcuraArquivo/ProcuraArquivo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ProcuraArquivo
+{
+    class ProcuraArquivo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com a pasta, a partir da qual será feita a pesquisa: ");
+            string pasta = Console.ReadLine();
+            Console.WriteLine("Entre com o nome do arquivo ou a máscara, para mais de um arquivo, exm: *.txt");
+            string mascara = Console.ReadLine();
+
+            var files = Directory.EnumerateFiles(pasta, mascara, SearchOption.AllDirectories);
+            foreach (var item in files)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
